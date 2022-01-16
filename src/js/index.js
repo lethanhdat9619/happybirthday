a0a4fb0f06e88f57c8ed8e7bad33332c48a41545
unknown <dat.lethanh@amela.vn>
2022-01-17 01:22:31 +0700

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,42 +1,58 @@
 
-let firstSlideContainer = 
-document.getElementsByClassName("slide--content")[0];
+let firstSlideContainer =
+	document.getElementsByClassName("slide--content")[0];
 
-let secondSlideContainer = 
-document.getElementsByClassName("slide--content--one")[0];
+let secondSlideContainer =
+	document.getElementsByClassName("slide--content--one")[0];
 
-let secondCanvas = 
-document.getElementsByClassName("second--canvas")[0];
+let secondCanvas =
+	document.getElementsByClassName("second--canvas")[0];
 
-let centerLine = 
-document.getElementsByClassName("center--line")[0];
+let centerLine =
+	document.getElementsByClassName("center--line")[0];
 
-secondSlideContainer.setAttribute("style","display:none");
-secondCanvas.setAttribute("style","display:none");
+let mail = document.getElementsByClassName('envelope')[0]
 
-let thirdCanvas = 
-document.getElementsByClassName("third--canvas")[0];
+let fire = document.getElementsByClassName('fire')[0]
 
-thirdCanvas.setAttribute("style","display:none");
+secondSlideContainer.setAttribute("style", "display:none");
+secondCanvas.setAttribute("style", "display:none");
 
+let thirdCanvas =
+	document.getElementsByClassName("third--canvas")[0];
 
-
-let containerToggleOne = setTimeout(function(){
-	firstSlideContainer.setAttribute("style","display:none");
-	secondSlideContainer.setAttribute("style","display:block");
-},4500);
+thirdCanvas.setAttribute("style", "display:none");
 
 
-let removeFirstSlide = setTimeout(function(){
-	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
-	secondCanvas.setAttribute("style","display:block");
-},8500);
 
-let removeSecondCanvas = setTimeout(function(){
-	secondCanvas.setAttribute("style","display:none");
-	thirdCanvas.setAttribute("style","display:block")
-},12800)
+let containerToggleOne = setTimeout(function () {
+	firstSlideContainer.setAttribute("style", "display:none");
+	secondSlideContainer.setAttribute("style", "display:block");
+}, 5000);
 
-let removeText = setTimeout(function(){
-	centerLine.setAttribute("style","display:none");
-},22800)
+
+let removeFirstSlide = setTimeout(function () {
+	document.getElementsByClassName("first--slide")[0].setAttribute("style", "display:none;")
+	secondCanvas.setAttribute("style", "display:block");
+}, 11500);
+
+let removeSecondCanvas = setTimeout(function () {
+	secondCanvas.setAttribute("style", "display:none");
+	thirdCanvas.setAttribute("style", "display:block")
+	mail.setAttribute("style", "display:none");
+}, 17000)
+
+let removeText = setTimeout(function () {
+	centerLine.setAttribute("style", "display:none");
+	mail.setAttribute("style", "display:block");
+}, 26800)
+
+let test = setTimeout(function () {
+		mail.classList.remove('new')
+		mail.classList.add('open')
+}, 28800)
+
+fire.addEventListener("click", () => {
+	mail.setAttribute("style", "display:none");
+})
+
